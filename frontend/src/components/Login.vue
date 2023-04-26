6bc2d01158a8036cd9a47fb90398dc1def8916b4
Vinicius Nascimento <vinicius.nascimento09@outlook.com>
2023-04-25 21:15:09 -0300

    fix: some fixes]

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,27 @@
+<template>
+  <form>
+    <div>
+    <label for="username">Usuário</label>
+    <input id="username" type="text" v-model="username" />
+
+    <label for="password">Senha</label>
+    <input id="password" type="text" v-model="password" />
+  </div>
+  <div>
+    <button type="submit">
+      Entrar
+    </button>
+  </div>
+  </form>
+</template>
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      username: '',
+      password: '',
+    }
+  }
+}
+</script>
